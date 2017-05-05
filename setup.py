b152bcabb57ecc6b0c7413fffab349b838e2a6a5
Dustin Zacharias <dustinzacharias@eduroamstud-130-82-253-47.unisg.ch>
2017-05-05 19:23:14 +0200

    first commit

--- setup.py
+++ setup.py
@@ -3,12 +3,13 @@
 from setuptools import setup
 
 setup(name='hngoption',
-      version='0.1',
+      version='0.5',
       description='Heston Nandi GARCH Option Pricing Model (2000)',
       url='https://github.com/SW71X/hngoption2',
-      download_url='https://github.com/SW71X/hngoption2/archive/0.1.tar.gz',
+      download_url='https://github.com/SW71X/hngoption2/archive/0.5.tar.gz',
       author='Dustin L. Zacharias',
       author_email='dustinl.zacharias@gmail.com',
       license='MIT',
       packages=['hngoption'],
       zip_safe=False)
+
